a5955e27ffbf98978be8dac8341581d39ae93f18
simon <simon.lousky@campus.technion.ac.il>
2019-12-26 13:43:10 +0200

    parser arguments added to train sub command
    reference file added

--- ModelLeaf.py
+++ ModelLeaf.py
@@ -1,15 +1,44 @@
 import argparse
 from ModelLeafInfer import infer
+from ModelLeafTrain import train
+from Reference import HelpReference
 
 def main():
     # top level parser
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=HelpReference.description, add_help=False)
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help=HelpReference.help_description)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+
+
     subparsers = parser.add_subparsers()
+    # parser for train
+    parser_train = subparsers.add_parser('train')
+    parser_train.add_argument('-o', '--output', help=HelpReference.TrainReference.output, default='./')
+    parser_train.add_argument('-k', '--dataset-keep', type=int, help=HelpReference.TrainReference.dataset_keep, default=0)
+    parser_train.add_argument('-t', '--test-set', help=HelpReference.TrainReference.test_set)
+    parser_train.add_argument('-c', '--config', help=HelpReference.TrainReference.config)
+    parser_train.add_argument('-s', '--synthetic', choices=['random', 'grouped'], help=HelpReference.TrainReference.synthetic)
+    parser_train.add_argument('--leaf-size-min', type=int, help=HelpReference.TrainReference.leaf_size_min)
+    parser_train.add_argument('--leaf-size-max', type=int, help=HelpReference.TrainReference.leaf_size_max)
+    parser_train.add_argument('--leaf-number-min', type=int, help=HelpReference.TrainReference.leaf_number_min)
+    parser_train.add_argument('--leaf-number-max', type=int, help=HelpReference.TrainReference.leaf_number_max)
+    parser_train.add_argument('--preview-only', type=int, help=HelpReference.TrainReference.preview_only)
+    parser_train.set_defaults(func=train)
 
     # parser for infer
     parser_infer = subparsers.add_parser('infer')
     parser_infer.add_argument('-x', type=int, default=1)
     parser_infer.set_defaults(func=infer)
+
+    # parser for cut
+    parser_train = subparsers.add_parser('cut')
+    # parser_train.add_argument('-y', type=int, default=2)
+    parser_train.set_defaults(func=train)
+
+    # parser for info
+    parser_train = subparsers.add_parser('info')
+    # parser_train.add_argument('-y', type=int, default=2)
+    parser_train.set_defaults(func=train)
 
     args = parser.parse_args()
     args.func(args)
---
+++ Reference.py
@@ -0,0 +1,54 @@
+
+class HelpReference:
+
+    description = "\
+    Model-leaf uses the Tensorflow implementation of Mask-RCNN by MatterPort and a handful of integration scripts and utilities to simplify training and inference of leaf datasets.\
+    For information on the different subcommands read the according manual pages\
+    "
+
+    help_description = "\
+    Prints the synopsis and the list of possible options and commands."
+
+    class TrainReference:
+        description = "Creates a dataset of synthetic pictures, and runs the training model on the dataset. The best result model is saved as a .h5 file."
+        
+        output = "specify path to .h5 model location [default: current]"
+
+        dataset_keep = "specify how many samples to keep [default: 0]"
+
+        test_set = "specify path to test set"
+
+        config = "specify path to the model (mask-r cnn) config file"
+
+        synthetic = "Set the synthetic dataset generator to scatter the leaves randomly (cucumber), or to group the leaves around a base (banana)"
+
+        leaf_size_min = "Set the minimum size of leaves in the synthetic picture"
+        
+        leaf_size_max = "Set the maximum size of leaves in the synthetic picture"
+
+        leaf_number_min = "Set the minimum number of leaves in the synthetic picture"
+
+        leaf_number_max = "Set the maximum number of leaves in the synthetic picture"
+
+        preview_only = "generate samples of training set without training the model"
+
+    class InferReference:
+        description = "Loads a dataset, loads a model, runs inference on all the pictures located in a directory. Outputs a set of pictures with a translucent mask on every detected leaf. Additionally, a json annotation file is generated."
+
+        output = "Set output directory [default: current]"
+
+        pictures_only = "Create only infered pictures with colorful transparent masks"
+
+        contour_only = "Create contour annotation file only"
+
+    class CutReference:
+        description = "Cut single leaf pictures from an annotated dataset"
+
+        normalize = "Normalize the generated pictures to the specified width-size. By default pictures are not normalized, every leaf stays at its original size"
+
+        background = "Specify the RGB background of the leaf, regardless of the alpha channel value [default: original]"
+
+        no_alpha = "Do not apply alpha around objects"
+
+    class InfoReference:
+        description = "Prints information about the model saved in the model-info variable"
