a38cc9e1520b972c42d5b65419641961d2eff2e1
Simon Lousky <simon.lousky@singular.net>
2020-03-02 21:39:11 +0200

    dockerfile c hanges

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,4 @@
 
 mkdir -p workspace
 cd workspace
-jupyter notebook --port=8888 --ip=0.0.0.0 --NotebookApp.token=
+conda run -n model-env jupyter notebook --port=8888 --ip=0.0.0.0 --NotebookApp.token=
