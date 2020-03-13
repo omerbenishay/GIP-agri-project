c08e0ff973261811e35075a771e48adb9ec7dad6
Simon Lousky <simon.lousky@singular.net>
2020-03-13 22:58:40 +0200

    setup update

--- setup.sh
+++ setup.sh
@@ -2,18 +2,20 @@
 ENV_NAME=leafsegmentor
 
 if [[ $(echo `conda env list | grep $ENV_NAME | awk '{print $1}'`) != $ENV_NAME ]] ; then
-  conda env create --name $ENV_NAME -f environment.yml;
+  conda env create --name $ENV_NAME -v -f environment.yml;
   conda init
 fi
 
-echo "alias leaf-segmentor='conda activate $ENV_NAME && python LeafSegmentor.py'" >> ~/.bash_profile
+echo "alias leafsegmentor='conda activate $ENV_NAME && python LeafSegmentor.py'" >> ~/.bash_profile
 
 . ~/.bash_profile
 
-if [[ ! -d ../models ]] then
-  cp -r /mnt/gluster/shares/WP2_Analysis/WP2_models/Kimmel/LeafSegmentor_data/models ..
+if [[ ! -d ../models ]]; then
+  echo "copying model files..."
+  rsync --info=progress2 -r /mnt/gluster/shares/WP2_Analysis/WP2_models/Kimmel/LeafSegmentor_data/models ..
 fi
 
-if [[ ! -d ../cut_jobs ]] then
-  cp -r /mnt/gluster/shares/WP2_Analysis/WP2_models/Kimmel/LeafSegmentor_data/cut_jobs ..
+if [[ ! -d ../cut_jobs ]]; then
+  echo "copying example cut jobs..."
+  rsync --info=progress2 -r /mnt/gluster/shares/WP2_Analysis/WP2_models/Kimmel/LeafSegmentor_data/cut_jobs ..
 fi
