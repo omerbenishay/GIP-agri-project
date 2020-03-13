9703651679949caa14771863dfff08a6f2fa8bc5
Simon Lousky <simon.lousky@singular.net>
2020-03-13 21:12:46 +0200

    setup update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
-if [[ $(echo `conda env list | grep model-cmd | awk '{print $1}'`) != "model-cmd" ]] ; then
-  conda env create -f environment.yml;
+
+ENV_NAME=leafsegmentor
+
+if [[ $(echo `conda env list | grep $ENV_NAME | awk '{print $1}'`) != $ENV_NAME ]] ; then
+  conda env create --name $ENV_NAME -f environment.yml;
   conda init
 fi
 
-echo "alias leaf-segmentor='conda activate model-cmd && python LeafSegmentor.py'" >> ~/.bash_profile
-echo "alias leaf-segmentor='conda activate model-cmd && python LeafSegmentor.py'" >> ~/.bashrc
+echo "alias leaf-segmentor='conda activate $ENV_NAME && python LeafSegmentor.py'" >> ~/.bash_profile
 
 . ~/.bash_profile
-. ~/.bashrc
+
+if [[ ! -d ../models ]] then
+  cp -r /mnt/gluster/shares/WP2_Analysis/WP2_models/Kimmel/LeafSegmentor_data/models ..
+fi
+
+if [[ ! -d ../cut_jobs ]] then
+  cp -r /mnt/gluster/shares/WP2_Analysis/WP2_models/Kimmel/LeafSegmentor_data/cut_jobs ..
+fi
